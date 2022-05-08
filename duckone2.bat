0f34fa9816fb5ed9eb19a2849ee1bd42bef376d0
wk2poor <81670847+wk2poor@users.noreply.github.com>
2022-05-08 16:45:14 +1000

    Add files via upload

---
+++ duckone2.bat
@@ -0,0 +1,89 @@
+
+
+@echo off
+title Duckone2 Activator d(TUT)b
+cls
+color 01
+echo ###############################################
+echo ######## Welcome To Duckone2 activator ########
+echo ### Here are the supported windows products ###
+echo ###############################################
+echo # Windows 10 Home                             #
+echo # Windows 10 Enterprise And LTSB              #	
+echo # Windows 10 Professional                     #
+echo # Windows 10 Education                        #  
+echo # Win11 Not tested ( USE AT YOUR OWN RISK )   #
+echo ###############################################
+echo To Start Activation Press any key
+echo.
+echo.
+pause > nul
+cls
+color 04
+echo ###############################################
+echo.
+echo Activating Windows Now...
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 2F77B-TNFGY-69QQF-B8YKP-D69TJ >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk WNMTR-4C88C-JK8YV-HQ7T2-76DF9 >nul
+color 02
+echo.
+echo DONE !
+echo.
+echo ################################################
+set i=1
+:server
+if %i%==1 set KMS_Sev=kms.shuax.com
+if %i%==2 set KMS_Sev=NextLevel.uk.to
+if %i%==3 set KMS_Sev=GuangPeng.uk.to
+if %i%==4 set KMS_Sev=AlwaysSmile.uk.to
+if %i%==5 set KMS_Sev=kms.chinancce.com 
+if %i%==6 exit
+cscript //nologo c:\windows\system32\slmgr.vbs /skms %KMS_Sev% >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ato | find /i "successfully" 
+echo.
+echo.
+choice /n /c YN /m "Would you like to restart your system now? ( Y . N )?" 
+if errorlevel 2 exit & goto server2
+:server2 
+cls
+color 04
+echo Server Connection Failed / connecting to another one... 
+cls
+echo Server Connection Failed | connecting to another one... 
+cls
+echo Server Connection Failed \ connecting to another one... 
+cls
+echo Server Connection Failed / connecting to another one... 
+cls
+echo Server Connection Failed | connecting to another one... 
+cls
+echo Server Connection Failed \ connecting to another one... 
+cls
+echo Server Connection Failed / connecting to another one... 
+cls
+echo Server Connection Failed | connecting to another one... 
+cls
+echo Server Connection Failed \ connecting to another one... 
+cls
+echo Server Connection Failed / connecting to another one... 
+cls
+echo Server Connection Failed | connecting to another one... 
+cls
+echo Server Connection Failed \ connecting to another one... 
+cls
+echo Please wait... 
+echo. 
+echo. 
+set /a i+=1 
+goto server
+shutdown.exe /r /t 00
